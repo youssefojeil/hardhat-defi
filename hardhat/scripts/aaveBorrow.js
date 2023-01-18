8050326fd3ea9124af52f83b0b87c4c570545aa7
unknown <youssef.ojeil@hotmail.com>
2023-01-17 22:37:50 -0500

    setup Aave Lendingpool interface, fork mainnet for weth contract

--- hardhat/scripts/aaveBorrow.js
+++ hardhat/scripts/aaveBorrow.js
@@ -1,6 +1,18 @@
+const { getNamedAccounts } = require("hardhat");
+const { getWeth } = require("../scripts/getWeth");
+
 async function main() {
   //protocol treats everything as an erc20
+
+  await getWeth();
+  const { deployer } = await getNamedAccounts();
+  // abi and contract address for aave
+
+  // lending pool address provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+  // lending pool:
 }
+
+async function getLendingPool() {}
 
 main()
   .then(() => {
