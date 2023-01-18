ec6511bd520a39b2fc7b3175082a8cf24a6d3c09
unknown <youssef.ojeil@hotmail.com>
2023-01-17 22:11:11 -0500

    install and setup hardhat and dependencies

---
+++ hardhat/scripts/aaveBorrow.js
@@ -0,0 +1,12 @@
+async function main() {
+  //protocol treats everything as an erc20
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
